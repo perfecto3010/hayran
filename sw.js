889b47ef851dd0611b92dad1d513b9b4114accad
perfecto3010 <perfecto3010@gmail.com>
2026-03-21 01:35:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-self.addEventListener('fetch', function(event) {
-  // This can be empty for a basic app, 
-  // but it must exist to trigger the 'Install' prompt.
+const cacheName = 'lahalibo-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700;900&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetching assets
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
